8c249d15606ab2b3bfffe049be29696b50475c4e
Cristian Abello <cristian.abello@valpo.edu>
2019-01-16 19:52:43 +0000

    another typo

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 // .eslintrc is intentionally set for this example to throw an error if any files contain console.log statements statements 
-console.log('This code will not if staged to commit');
+console.log('This code will fail if staged to commit');
